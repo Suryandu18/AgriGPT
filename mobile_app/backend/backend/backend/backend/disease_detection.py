b3df7ebed733033350a59d926edd27b713a211f7
Suryandu18 <suryandugy@gmail.com>
2026-04-23 19:32:21 +0530

    Create disease_detection.py

---
+++ mobile_app/backend/backend/backend/backend/disease_detection.py
@@ -0,0 +1,9 @@
+def detect_disease(image):
+    # Temporary logic
+    result = {
+        "plant": "Tomato",
+        "disease": "Early Blight",
+        "solution": "Spray fungicide and remove infected leaves"
+    }
+    
+    return result
