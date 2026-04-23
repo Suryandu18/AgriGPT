bd8f1c50a95bfd6280a3f3e06d9d676a4d955941
Suryandu18 <suryandugy@gmail.com>
2026-04-23 19:33:40 +0530

    Update main.py

--- mobile_app/backend/backend/main.py
+++ mobile_app/backend/backend/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from disease_detection import detect_disease
+from chatbot import ask_agrigpt
 
 app = FastAPI()
 
@@ -9,3 +11,20 @@ def home():
 @app.get("/health")
 def health_check():
     return {"status": "Server running"}
+
+# Plant Disease Detection API
+@app.post("/detect-disease")
+async def detect(image: UploadFile = File(...)):
+    
+    result = detect_disease(image)
+    
+    return result
+
+
+# Agriculture Chatbot API
+@app.get("/ask-agrigpt")
+def ask(question: str):
+    
+    answer = ask_agrigpt(question)
+    
+    return {"response": answer}
